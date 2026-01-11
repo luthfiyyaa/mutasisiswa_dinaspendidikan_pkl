9607a15ebb869662a4d27f38bdd2ca42deac9397
Luthfiyya Ayu <luthfiyyaaayu@gmail.com>
2026-01-11 18:40:55 +0700

    fifi - update model

--- app/Mutasi.php
+++ app/Mutasi.php
@@ -6,6 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mutasi extends Model
 {
-  protected $table = 'mutasi';
-  protected $primaryKey = 'mutasi_id';
+    protected $table = 'mutasi';
+    protected $primaryKey = 'mutasi_id';
+    
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [];
+    
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [];
+    }
 }
